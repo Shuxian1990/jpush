3f62101044a9779387d0fd146f7dab0f7f9b1660
shuxian <shuxian@huize.com>
2018-08-25 10:55:43 +0800

    注册管理员单元测试

--- im_test/im_test.go
+++ im_test/im_test.go
@@ -15,7 +15,8 @@ func Test_RegisterUser(t *testing.T) {
 		AppKey:       "你的appKey",
 		MasterSecret: "你的MasterSecret",
 	}
-	c := im.Init(ini)
+	c, err := im.Init(ini)
+	assert.Nil(t, err)
 
 	var users = []im.User{{UserName: "asdfw3dfas23sdf", Password: "asdfw3dfas23sdf2"}}
 
@@ -23,4 +24,20 @@ func Test_RegisterUser(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Nil(t, rsp[0].Error)
+}
+
+// Test_RegisterAdmin 测试注册管理员
+func Test_RegisterAdmin(t *testing.T) {
+	ini := im.InitParams{
+		AppKey:       "3ac491b5b80577bb48503e12",
+		MasterSecret: "388c2a202c6368a9186e26db",
+	}
+	c, err := im.Init(ini)
+	assert.Nil(t, err)
+
+	var user = im.User{UserName: "asdfw3dfa98ad12", Password: "asdfw3dfas23sdf2"}
+
+	_, err = c.RegisterAdmin(user)
+	assert.Nil(t, err)
+
 }
